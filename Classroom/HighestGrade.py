98875cbe64953c3e94fe013d3b8d13455a81fd84
Kevin Aloysius <kevinaloys@rocketmail.com>
2014-02-22 01:41:22 -0800

    Added Solution to Higest Grade

---
+++ Classroom/HighestGrade.py
@@ -0,0 +1,22 @@
+#
+#
+# To find minimum and maximum in an array
+#
+__author__="Kevin Aloysius"
+def minmax(a):
+
+	max = a[0]
+	min = a[0]
+	for i in range(1,len(a)):
+		if(a[i]>max):
+			max = a[i]
+		elif(a[i]<min):
+			min = a[i]
+	print "Min ",min
+	print "Max", max
+
+
+
+
+a=[59,8,4,5,65,4,5,5,4,5,6,21,2,6,5,48,786,5,45,4,11]
+minmax(a)
