0ca5696f2430cd5687007adb9adf0a792b9fe238
Kevin Aloysius <kevinaloys@rocketmail.com>
2014-04-14 00:51:16 -0700

    Created Corpus Generation Definition for segretaing words from the sample text

--- Data_Statistics/bigram.py
+++ Data_Statistics/bigram.py
@@ -3,17 +3,28 @@
 def open_file(filename):
 	f = open(filename,'rU')
 	filenames = f.readlines()
+	if filenames == None:
+		raise Excpetion("Nothing to read in file")
 	sentence = []
 
 	for string in filenames:
-		string = string.lower()
-		string = string.strip()
+		string = string.lower() # Change string within the list into lower case for the sake of uniformity
+		string = string.strip()	# Remove the trailing \n from the end of the sentence
 		sentence.append(string)
 	return sentence
-		
 
+def corpus_generation(sentence_list):
+	corpus_list = []
+	for sentence in sentence_list:
+		strings = sentence.split(' ')
+		for string in strings:
+			corpus_list.append(string)
+
+	return corpus_list
 def main():
-	open_file('sample_text.txt')
+
+	sentence_list = open_file('sample_text.txt')
+	print corpus_generation(sentence_list)
 
 if __name__ == "__main__":
 	main()
