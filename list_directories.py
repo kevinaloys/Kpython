53c118f00f7eec4947f60493928db34d52923ec3
Kevin Aloysius <kevinaloys@rocketmail.com>
2014-04-22 01:27:01 -0700

    Added Solution

--- list_directories.py
+++ list_directories.py
@@ -1,9 +1,10 @@
 import os
 
+__author__ = "Kevin Aloysius"
 def list_dir(directory_location):
 	print "Parent Directory +", directory_location
 	print "Subdirectory:"
 	for each_dirctorty in os.listdir(directory_location):
 		print "-",each_dirctorty
 
-list_dir('/')
+list_dir('/dev')
